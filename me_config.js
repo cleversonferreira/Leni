4402f8c1a77c30961da4a96c30f066b4ed57e5da
Cleverson <cleverson_reisferreira@hotmail.com>
2017-09-27 19:09:50 -0300

    First commit

--- me_config.js
+++ me_config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "mqtt": {
     host: "mqtt://YOUR_HOST_MQTT",
-    port: 0000,
+    port: 8883,
     clientId: "leni_" + Math.random().toString(16).substr(2, 8),
     username: "YOUR_USERNAME",
     password: "YOUR_PASSWORD",
