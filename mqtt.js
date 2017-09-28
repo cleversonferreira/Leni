ad071508bb51658a55146b173f1536ecd9c50e78
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-28 12:16:30 -0300

    Improvement - Added eslint, editorconfig and improvement mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,23 +1,19 @@
 const config = require('./config');
 const mqttClient = require('mqtt');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
-MongoClient.connect(config.mongo.host, (err, database) => {
-  if (err) return console.log(err);
+MongoClient.connect(config.mongo.host, (err, db) => {
+  if (err) console.log(err);
 
-  let client = mqttClient.connect(config.mqtt.host, config.mqtt);
+  const client = mqttClient.connect(config.mqtt.host, config.mqtt);
 
   client.on('connect', () => {
     client.subscribe('log/#', () => {
       client.on('message', (topic, message) => {
-
-        console.log("Received '" + message + "' on '" + topic + "'");
-        database.collection('notes').insert(JSON.parse(message), (err) => {
-          if (err) return console.log("error: " + err);
-            console.log("log successfully registered");
-          });
+        console.log(`Received '${message}' on '${topic}'`);
+        db.collection('notes').insert(JSON.parse(message), e =>
+          ((e) ? console.log(`error: ${err}`) : console.log('log successfully registered')));
       });
     });
   });
-
 });
