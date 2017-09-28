e330488efd7763041052c59af7504791cd41afc8
Cleverson Ferreira <cleverson_reisferreira@hotmail.com>
2017-09-28 12:48:56 -0300

    Revert "Improvement - Added eslint, editorconfig and improvement mqtt.js"

--- me_config.js
+++ me_config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  mqtt: {
-    host: 'mqtt://YOUR_HOST_MQTT',
+  "mqtt": {
+    host: "mqtt://YOUR_HOST_MQTT",
     port: 8883,
-    clientId: `leni_${Math.random().toString(16).substr(2, 8)}`,
-    username: 'YOUR_USERNAME',
-    password: 'YOUR_PASSWORD',
+    clientId: "leni_" + Math.random().toString(16).substr(2, 8),
+    username: "YOUR_USERNAME",
+    password: "YOUR_PASSWORD",
     keepalive: 60,
     reconnectPeriod: 1000,
-    protocolId: 'MQIsdp',
+    protocolId: "MQIsdp",
     protocolVersion: 2,
     clean: true,
-    encoding: 'utf8',
+    encoding: "utf8"
   },
-  mongo: {
-    host: 'YOUR_MONGO_URL',
-  },
+  "mongo": {
+    "host": "YOUR_MONGO_URL"
+  }
 };
